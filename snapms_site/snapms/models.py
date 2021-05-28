46de21e408c606922f3b0f3710add2f67a138e82
jvansan <jeffreyavansanten@gmail.com>
2021-05-27 19:32:34 -0700

    adds Job DB model

--- snapms_site/snapms/models.py
+++ snapms_site/snapms/models.py
@@ -1,3 +1,28 @@
+import uuid
+from enum import Enum
 from django.db import models
 
-# Create your models here.
+
+class Status(str, Enum):
+    queued = "queued"
+    running = "running"
+    failed = "failed"
+    completed = "completed"
+
+
+class Job(models.Model):
+    """Model for tracking SnapMS Jobs.
+    Automatically creates a UUID and sets status to queued when created.
+
+    Tracks date created, archived (for when over expiry date: probably 6 months),
+    status, inputfile name, and parameters used.
+    """
+
+    id = models.CharField(
+        primary_key=True, default=str(uuid.uuid4()), editable=False, max_length=32
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
+    status = models.CharField(max_length=128, default=Status.queued.value)
+    inputfile = models.CharField(max_length=256)
+    parameters = models.TextField()
--- snapms_site/snapms/tests.py
+++ snapms_site/snapms/tests.py
@@ -1,3 +1,75 @@
+from http import HTTPStatus
+from uuid import UUID
+import json
+
+from django.shortcuts import resolve_url
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Job, Status
+
+
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+
+     Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+
+     Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+
+     Examples
+    --------
+    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
+    True
+    >>> is_valid_uuid('c9bf9e58')
+    False
+    """
+
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
+
+
+class SimpleViewTests(TestCase):
+    """
+    Simple tests to make sure the URLs for each "simple" webpage is where
+    it should be.
+    """
+
+    def test_dashboard(self):
+        response = self.client.get(resolve_url("snapms:dashboard"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_docs(self):
+        response = self.client.get(resolve_url("snapms:docs"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_handle_snapms_is_not_get(self):
+        response = self.client.get(resolve_url("snapms:handle_snapms"))
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def test_handle_snapms_is_post(self):
+        response = self.client.post(resolve_url("snapms:handle_snapms"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class JobModelTests(TestCase):
+    def test_job_creates_uuid(self):
+        job = Job.objects.create(
+            inputfile="test",
+            parameters=json.dumps({"test": "test"}),
+        )
+        self.assert_(is_valid_uuid(job.id))
+
+    def test_job_create_sets_status_queued(self):
+        job = Job.objects.create(
+            inputfile="test",
+            parameters=json.dumps({"test": "test"}),
+        )
+        self.assertEqual(job.status, Status.queued.value)
